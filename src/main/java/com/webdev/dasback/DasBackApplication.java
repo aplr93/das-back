40979957b5df33f68c9f5cb513d4a9532589275e
Bruno <bruno.campera@serpro.gov.br>
2021-10-13 20:46:02 -0300

    Initializes project

---
+++ src/main/java/com/webdev/dasback/DasBackApplication.java
@@ -0,0 +1,13 @@
+package com.webdev.dasback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasBackApplication.class, args);
+	}
+
+}
