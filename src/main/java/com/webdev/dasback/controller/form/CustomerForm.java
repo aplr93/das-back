abf1b42b5107598c8bc402d0d8beb79911aebca9
Bruno <bruno.campera@serpro.gov.br>
2021-10-13 22:32:17 -0300

    Creates customer form model for creating customers

---
+++ src/main/java/com/webdev/dasback/controller/form/CustomerForm.java
@@ -0,0 +1,51 @@
+package com.webdev.dasback.controller.form;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.webdev.dasback.model.Customer;
+import com.webdev.dasback.repository.CustomerRepository;
+
+public class CustomerForm {
+	
+	@NotNull @NotEmpty @Pattern(regexp="\\d{11}")	
+	private String cpf;
+	@NotNull @NotEmpty @Length(min = 2)
+	private String firstName;
+	@NotNull @NotEmpty @Length(min = 2)
+	private String lastName;
+
+	public Customer convertToCustomer() {
+		return new Customer(cpf, firstName, lastName);
+	}
+	
+	public Customer update(Long id, CustomerRepository customerRepository) {
+		Customer customer = customerRepository.getOne(id);
+		customer.setCpf(this.cpf);
+		customer.setFirstName(this.firstName);
+		customer.setLastName(this.lastName);		
+		return customer;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
