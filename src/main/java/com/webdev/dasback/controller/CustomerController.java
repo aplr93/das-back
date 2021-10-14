6c14d657111bd6da5f0dac32c4a3b056e856749f
Bruno <bruno.campera@serpro.gov.br>
2021-10-14 00:38:23 -0300

    Creates endpoint for deleting customers

--- src/main/java/com/webdev/dasback/controller/CustomerController.java
+++ src/main/java/com/webdev/dasback/controller/CustomerController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,6 +60,17 @@ public class CustomerController {
 		if (customerOptional.isPresent()) {
 			Customer customer = form.update((long)id, customerRepository);
 			return ResponseEntity.ok(customer);
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@Transactional
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> remover(@PathVariable Long id) {
+		Optional<Customer> topicoOptional = customerRepository.findById(id);
+		if (topicoOptional.isPresent()) {
+			customerRepository.deleteById(id);
+			return ResponseEntity.ok().build();
 		}
 		return ResponseEntity.notFound().build();
 	}
