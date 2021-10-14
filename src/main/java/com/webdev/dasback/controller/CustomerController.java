28c697ff748343b5dd26d9077feaff1e3a8ec1d2
Bruno <bruno.campera@serpro.gov.br>
2021-10-14 00:09:20 -0300

    Create customer creation endpoint

--- src/main/java/com/webdev/dasback/controller/CustomerController.java
+++ src/main/java/com/webdev/dasback/controller/CustomerController.java
@@ -2,11 +2,16 @@ package com.webdev.dasback.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.webdev.dasback.controller.form.CustomerForm;
 import com.webdev.dasback.model.Customer;
 import com.webdev.dasback.repository.CustomerRepository;
 
@@ -17,9 +22,14 @@ public class CustomerController {
 	@Autowired
 	private CustomerRepository customerRepository;
 	
-	
 	@GetMapping
 	public List<Customer> list(){
 		return customerRepository.findAll();
+	}
+	
+	@PostMapping
+	public void cadastrar(@RequestBody @Valid CustomerForm customerForm) {
+		Customer customer = customerForm.convertToCustomer();
+		customerRepository.save(customer);
 	}
 }
