63c3405c4d8da0ceb9f47b7f5a06bc102c736c12
Bruno <bruno.campera@serpro.gov.br>
2021-11-23 20:22:44 -0300

    Forbids deleting customer with orders placed

--- src/main/java/com/webdev/dasback/controller/CustomerController.java
+++ src/main/java/com/webdev/dasback/controller/CustomerController.java
@@ -10,12 +10,22 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.webdev.dasback.controller.form.CustomerForm;
 import com.webdev.dasback.model.Customer;
+import com.webdev.dasback.model.Order;
 import com.webdev.dasback.repository.CustomerRepository;
+import com.webdev.dasback.repository.OrderRepository;
 
 @RestController
 @RequestMapping("/customers")
@@ -23,7 +33,10 @@ public class CustomerController {
 
 	@Autowired
 	private CustomerRepository customerRepository;
-	
+
+	@Autowired
+	private OrderRepository orderRepository;
+
 	@GetMapping
 	public List<Customer> getCustomer(@RequestParam(required=false, name="cpf") String cpf){
 		if (cpf == null) {
@@ -33,7 +46,7 @@ public class CustomerController {
 			return customerRepository.findByCpf(cpf);
 		}
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<Customer> getCustomerById(@PathVariable Long id) {
 		Optional<Customer> customerOptional = customerRepository.findById(id);
@@ -47,14 +60,14 @@ public class CustomerController {
 	public ResponseEntity<Object> createCustomer(@RequestBody @Valid CustomerForm customerForm, UriComponentsBuilder uriBuilder) {
 		Customer customer = customerForm.convertToCustomer();
 
-		return validateAndProcessCustomerCreation(customer, uriBuilder); 		
+		return validateAndProcessCustomerCreation(customer, uriBuilder);
 	}
 
 	@PutMapping("/{id}")
 	@Transactional
 	public ResponseEntity<Object> updateCustomer(@PathVariable Long id, @RequestBody @Valid CustomerForm form) {
-		Optional<Customer> customerOptional = customerRepository.findById(id);	
-		return validateAndProcessCustomerUpdate(customerOptional, form);	
+		Optional<Customer> customerOptional = customerRepository.findById(id);
+		return validateAndProcessCustomerUpdate(customerOptional, form);
 	}
 
 	@Transactional
@@ -76,15 +89,22 @@ public class CustomerController {
 	private ResponseEntity<Object> buildCustomerRemovalErrorResponse(Optional<Customer> customerOptional) {
 		if( ! customerOptional.isPresent()) {
 			return ResponseEntity.notFound().build();
+		}else if(customerHasOrders(customerOptional.get())) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Este cliente não pode ser removido devido à existência de pedidos em seu nome.");
 		}
 		return null;
+	}
+
+	private boolean customerHasOrders(Customer customer) {
+		List<Order> customerOrderList = this.orderRepository.findByCustomerId(customer.getId());
+		return customerOrderList.size() > 0;
 	}
 
 	private ResponseEntity<Object> commitCustomerRemoval(Customer customer) {
 		customerRepository.deleteById(customer.getId());
 		return ResponseEntity.ok(customer);
 	}
-	
+
 	private ResponseEntity<Object> commitCustomerUpdate(Customer customer, CustomerForm form) {
 		form.updateEntity(customer);
 		return ResponseEntity.ok(customer);
@@ -104,11 +124,11 @@ public class CustomerController {
 	private boolean previousCpfWasModified(Optional<Customer> customerOptional, CustomerForm form) {
 		boolean returnValue = true;
 		if(customerOptional.isPresent()) {
-			returnValue = ! customerOptional.get().getCpf().equals(form.getCpf());  
+			returnValue = ! customerOptional.get().getCpf().equals(form.getCpf());
 		}
 		return returnValue;
 	}
-	
+
 	private boolean cpfAlreadyRegistered(String cpf) {
 		List<Customer> customersWithSameCpf = customerRepository.findByCpf(cpf);
 		return customersWithSameCpf.size() > 0;
@@ -116,29 +136,29 @@ public class CustomerController {
 
 	private ResponseEntity<Object> commitCustomerCreation(Customer customer, UriComponentsBuilder uriBuilder) {
 		customerRepository.save(customer);
-		
+
 		//Returns the same resource that was created as a JSON response to the front end
-		URI uri = uriBuilder.path("/customers/{id}").buildAndExpand(customer.getId()).toUri(); 
+		URI uri = uriBuilder.path("/customers/{id}").buildAndExpand(customer.getId()).toUri();
 		return ResponseEntity.created(uri).body(customer);
 	}
-	
+
 	private ResponseEntity<Object> validateAndProcessCustomerUpdate(Optional<Customer> customerOptional,
 			CustomerForm form) {
 		ResponseEntity<Object> errorResponse = buildCustomerUpdateErrorResponse(customerOptional, form);
 		if(errorResponse != null) {
 			return errorResponse;
 		}
-		
+
 		return commitCustomerUpdate(customerOptional.get(), form);
 	}
-	
+
 	private ResponseEntity<Object> validateAndProcessCustomerCreation(Customer customer,
 			UriComponentsBuilder uriBuilder) {
 		ResponseEntity<Object> errorResponse = buildCustomerCreationErrorResponse(customer);
 		if(errorResponse != null) {
 			return errorResponse;
 		}
-		
+
 		return commitCustomerCreation(customer, uriBuilder);
 	}
 
